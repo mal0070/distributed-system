5e6e1171ea99abf0b1dd7b502737dad6ec0e1bae
mal0070 <mal0070@naver.com>
2023-04-08 17:10:40 +0900

    6.2 setting

---
+++ CMApp/src/main/java/CMServerEventHandler.java
@@ -0,0 +1,38 @@
+//import kr.ac.konkuk.ccslab.cm.*;
+import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
+import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+
+public class CMServerEventHandler implements CMAppEventHandler {
+    private CMServerStub m_serverStub;
+    public CMServerEventHandler(CMServerStub serverStub)
+    {
+        m_serverStub = serverStub;
+    }
+    @Override
+    public void processEvent(CMEvent cme) {
+
+        switch(cme.getType())
+        {
+            case CMInfo.CM_SESSION_EVENT:
+                processSessionEvent(cme);
+                break;
+            default:
+                return;
+        }
+    }
+    private void processSessionEvent(CMEvent cme)
+    {
+        CMSessionEvent se = (CMSessionEvent) cme;
+        switch(se.getID())
+        {
+            case CMSessionEvent.LOGIN:
+                System.out.println("["+se.getUserName()+"] requests login.");
+                break;
+            default:
+                return;
+        }
+    }
+}
